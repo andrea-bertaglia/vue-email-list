5cdb4c9c579aaf840cea3c21c0c707efe62f17ab
andrea-bertaglia <andreabertaglia@gmail.com>
2024-04-29 17:17:54 +0200

    feat: emails api generation

--- js/script.js
+++ js/script.js
@@ -1,12 +1,29 @@
 // test
 console.log("Hello World!");
+// API https://flynn.boolean.careers/exercises/api/random/mail
 
 const { createApp } = Vue;
 
 createApp({
   data() {
     return {
-      message: "Hello Vue!",
+      emailsArray: [],
     };
+  },
+  created() {
+    for (let i = 0; i < 10; i++) {
+      this.generateArray();
+    }
+  },
+  methods: {
+    generateArray: function () {
+      axios
+        .get("https://flynn.boolean.careers/exercises/api/random/mail")
+        .then((apiResp) => {
+          console.log(apiResp);
+          this.emailsArray.push(apiResp.data.response);
+          console.log(this.emailsArray);
+        });
+    },
   },
 }).mount("#app");
