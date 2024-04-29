99c7becd217777a3f6dc0eff3748200358ccfbe4
andrea-bertaglia <andreabertaglia@gmail.com>
2024-04-29 18:43:21 +0200

    feat: loading message

--- js/script.js
+++ js/script.js
@@ -8,22 +8,23 @@ createApp({
   data() {
     return {
       emailsArray: [],
+      isLoading: true,
     };
   },
   created() {
-    for (let i = 0; i < 10; i++) {
-      this.generateArray();
-    }
+    this.generateArray();
   },
   methods: {
     generateArray: function () {
-      axios
-        .get("https://flynn.boolean.careers/exercises/api/random/mail")
-        .then((apiResp) => {
-          console.log(apiResp);
-          this.emailsArray.push(apiResp.data.response);
-          console.log(this.emailsArray);
-        });
+      for (let i = 0; i < 10; i++) {
+        axios
+          .get("https://flynn.boolean.careers/exercises/api/random/mail")
+          .then((apiResp) => {
+            console.log(apiResp);
+            this.emailsArray.push(apiResp.data.response);
+            this.isLoading = false;
+          });
+      }
     },
   },
 }).mount("#app");
